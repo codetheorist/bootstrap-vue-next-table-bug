149b16aba4e47948680a084af29a1b731fa46f1c
codetheorist <codetheorist@outlook.com>
2023-11-26 19:32:16 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <BCard>
+      <BForm>
+        <BFormGroup label="Search" label-cols="3" label-for="search-input">
+          <BFormInput v-model="search" id="search-input" />
+        </BFormGroup>
+      </BForm>
+      <BTable :filter="search" :provider="provider" show-empty />
+    </BCard>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+const search = ref('');
+const provider = (options) => {
+  console.log(options);
+
+  return [];
+};
+</script>
